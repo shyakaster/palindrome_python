4e32930ad30d343e6f6d2d564738cc171d9da1dc
alex shyaka <alex.nkusi@codeimpact.co>
2023-07-28 11:57:04 +0100

    Add results page and partial palindrome page and tests for checking palindrome functionality

--- palindrome_detector/__init__.py
+++ palindrome_detector/__init__.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from palindrome_shyakaster.phrase import Phrase
 
 
 def create_app(test_config=None):
@@ -22,16 +23,21 @@ def create_app(test_config=None):
 
     @app.route("/")
     def index():
-        return render_template("index.html")
+        return render_template("index.html", page_title="Home")
     
     @app.route("/about")
     def about():
-        return render_template("about.html")
+        return render_template("about.html",page_title="About")
     
     @app.route("/palindrome")
     def palindrome():
-        return render_template("palindrome.html")
-
+        return render_template("palindrome.html", page_title="Palindrome Detector")
+    @app.route("/check", methods=("POST",))
+    def check():
+        phrase=request.form["phrase"]
+        is_palindrome=Phrase(phrase).ispalindrome()
+        return render_template("results.html", 
+                               is_palindrome=is_palindrome,phrase=phrase)
     return app
 
 app = create_app()
